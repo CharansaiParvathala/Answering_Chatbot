c1424b90acfc0b45e74ddac018546f4f24f59755
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2025-02-12 14:30:05 +0530

    Update yt_transcript.py

--- yt_transcript.py
+++ yt_transcript.py
@@ -126,7 +126,7 @@ def main():
     pdfile = url = None
 
     # Single heading with colors for 'PDF' and 'YouTube'
-    st.markdown('<h1 style="font-size:36px;"><span style="color:blue;">PDF</span> & <span style="color:red;">Youtube</span> ChatBot</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="font-size:32px; text-align:center;"><span style="color:blue;">PDF</span> & <span style="color:red;">Youtube</span> ChatBot</h1>', unsafe_allow_html=True)
 
     # Sidebar description text without colors
     st.sidebar.write("""
