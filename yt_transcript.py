90b256c12f61c71faa016af54b6adb2f303745a2
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2025-02-12 13:46:09 +0530

    Update yt_transcript.py

--- yt_transcript.py
+++ yt_transcript.py
@@ -223,6 +223,6 @@ def main():
 
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
-    st.markdown(footer,unsafe_allow_html=True)
+    st.sidebar.markdown(footer,unsafe_allow_html=True)
 if __name__ == '__main__':
     main()
